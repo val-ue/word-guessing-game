532a99f5114f9331fab05f7c00f66c9fd32e88b8
valerie_l <contacttovalerie@gmail.com>
2023-04-13 18:34:14 -0400

    Added guess click event

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ const guessedLetters = document.querySelector(".guessed-letters");
 //list where the player’s guessed letters will appear
 const guessButton = document.querySelector(".guess");
 //button with the text “Guess!”
-const guessInput = document.querySelector(".letter");
+const textInput = document.querySelector(".letter");
 //text input where the player will guess a letter
 const wordInProgress = document.querySelector(".word-in-progress");
 //where the word in progress will appear
 const remaining = document.querySelector(".remaining");
 //where the remaining guesses will display
-const guessSpan = document.querySelector(".remaining span");
+const remainingSpan = document.querySelector(".remaining span");
 //span in remaining guesses paragraph
 const message = document.querySelector(".message");
 //where messages appear after player guesses a letter
@@ -18,17 +18,20 @@ const playAgain = document.querySelector(".play-again");
 const word = "magnolia";
 
 const progressUpdate = function (word) {
-
     const letterPlaceholder = [];
-    
-    for (const letters of word) {
-        console.log(letters);
+    for (const letter of word) {
+        console.log(letter);
         letterPlaceholder.push("●");
     }
-
     wordInProgress.innerText = letterPlaceholder.join("");
-
 };
+
+guessButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    const guess = textInput.value;
+    console.log(guess);
+    textInput.value = "";
+});
 
 progressUpdate(word);
 
